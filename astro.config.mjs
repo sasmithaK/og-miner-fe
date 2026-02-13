66449989d74fe6062d2f643ad43d54a0f7a452df
Sasmitha Kavindu <kavindugunasena@gmail.com>
2026-02-13 11:02:28 +0530

    fix: Fixed Paths for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sasmithak.github.io',
+  base: '/og-miner-fe',
   integrations: [tailwind()],
 });
